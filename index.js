c40fcf0f61a633f6143dae419e75658af336f047
Haythan <haythango123456789@gmail.com>
2025-05-17 15:44:18 -0400

    verify dom

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ app.use((req, res, next) => {
 app.get('/', (req, res) => {
     const sub = req.subdomain;
     console.log(sub.length < 4)
+    console.log(sub)
     // Si hay algún subdominio, redirigir a Facebook
     if (sub.length < 4) {
         return res.redirect('https://admin.sge.pt/MUA/1');
