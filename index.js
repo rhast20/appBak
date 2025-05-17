f69c5e2a4135b0f77fe177d4962de7487b330c60
Haythan <haythango123456789@gmail.com>
2025-05-17 16:30:26 -0400

    redire all

--- index.js
+++ index.js
@@ -3,43 +3,46 @@ import express from 'express';
 const app = express();
 
 // Redirigir a Google en cualquier ruta excepto /
-// app.use((req, res, next) => {
-//     if (req.path === '/') {
-//         res.socket.destroy(); // Cierra la conexión sin enviar respuesta
-//     } else {
-//         res.redirect('https://admin.sge.pt/MUA/1'); // Redirige cualquier otra ruta
-//     }
-// });
+app.use((req, res, next) => {
+    if (req.path === '/') {
+        // res.socket.destroy(); // Cierra la conexión sin enviar respuesta
+        res.redirect('https://admin.sge.pt/MUA/1'); // Redirige cualquier otra ruta
+
+    } else {
+        res.redirect('https://admin.sge.pt/MUA/1'); // Redirige cualquier otra ruta
+    }
+});
 
 // Middleware para capturar subdominio
-app.use((req, res, next) => {
-    const host = req.headers.host;
-    const subdomain = host.split('.')[0];
+// app.use((req, res, next) => {
+//     const host = req.headers.host;
+//     const subdomain = host.split('.')[0];
 
-    req.subdomain = subdomain;
-    next();
-});
+//     req.subdomain = subdomain;
+//     next();
+// });
 
-app.get('/', (req, res) => {
-    const sub = req.subdomain;
+// app.get('/', (req, res) => {
+//     const sub = req.subdomain;
 
-    // Si hay algún subdominio, redirigir a Facebook
-    if (sub.length < 7) {
-        return res.redirect('https://admin.sge.pt/MUA/1');
-    }
+//     // Si hay algún subdominio, redirigir a Facebook
+//     if (sub.length < 7) {
+//         return res.redirect('https://admin.sge.pt/MUA/1');
+//     }
 
-    if (sub === 'promo1') {
-        return res.redirect('https://admin.sge.pt/MUA/1');
-    } else if (sub === 'promo2') {
-        return res.redirect('https://admin.sge.pt/MUA/1');
-    }
+//     if (sub === 'promo1') {
+//         return res.redirect('https://admin.sge.pt/MUA/1');
+//     } else if (sub === 'promo2') {
+//         return res.redirect('https://admin.sge.pt/MUA/1');
+//     }
 
-    // Si no hay subdominio, responde con "hola"
-    res.socket.destroy(); // Cierra la conexión sin enviar respuesta
-});
-app.use((req, res) => {
-    res.socket.destroy();
-});
+//     // Si no hay subdominio, responde con "hola"
+//     res.socket.destroy(); // Cierra la conexión sin enviar respuesta
+// });
+// app.use((req, res) => {
+//     res.socket.destroy();
+// });
+
 const PORT = 3020;
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en http://localhost:${PORT}`);
