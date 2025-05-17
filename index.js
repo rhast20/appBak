006583c5c185f07745c14da6205223fb89ebb24b
Haythan <haythango123456789@gmail.com>
2025-05-17 15:30:42 -0400

    new redirec

--- index.js
+++ index.js
@@ -3,14 +3,37 @@ import express from 'express';
 const app = express();
 
 // Redirigir a Google en cualquier ruta excepto /
+// app.use((req, res, next) => {
+//     if (req.path === '/') {
+//         res.socket.destroy(); // Cierra la conexión sin enviar respuesta
+//     } else {
+//         res.redirect('https://admin.sge.pt/MUA/1'); // Redirige cualquier otra ruta
+//     }
+// });
+
+// Middleware para capturar subdominio
 app.use((req, res, next) => {
-    if (req.path === '/') {
-        res.socket.destroy(); // Cierra la conexión sin enviar respuesta
-    } else {
-        res.redirect('https://admin.sge.pt/MUA/1'); // Redirige cualquier otra ruta
-    }
+    const host = req.headers.host;
+    const subdomain = host.split('.')[0];
+
+    req.subdomain = subdomain;
+    next();
 });
 
+app.get('/', (req, res) => {
+    const sub = req.subdomain;
+    console.log(sub.length < 4)
+    // Si hay algún subdominio, redirigir a Facebook
+    if (sub.length < 4) {
+        return res.redirect('https://facebook.com');
+    }
+
+    // Si no hay subdominio, responde con "hola"
+    res.socket.destroy(); // Cierra la conexión sin enviar respuesta
+});
+app.use((req, res) => {
+  res.socket.destroy();
+});
 const PORT = 3020;
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en http://localhost:${PORT}`);
